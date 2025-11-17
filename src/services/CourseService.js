a3b1ba5f7d5cd13354ff4314b3de52c19288c7bf
JuanAguirre10 <juancho71096@gmail.com>
2025-11-17 16:56:41 -0500

    Update API URLs for production

--- src/services/CourseService.js
+++ src/services/CourseService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8081/api/courses';
+const API_URL = 'https://lab13-student-course-api.onrender.com/api/courses';
 
 class CourseService {
     getAllCourses() {
